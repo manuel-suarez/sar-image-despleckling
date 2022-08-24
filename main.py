c4998dedee2e92d7366a28f1c80d1ae772ee33a1
Arturo Suárez <manuel.suarez@cimat.mx>
2022-08-24 01:18:53 -0500

    Remove unused import

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import cv2
 import tensorflow as tf
 from tensorflow.keras.layers import Input,Conv2D,ReLU,BatchNormalization,LeakyReLU
 import os
