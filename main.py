ebcec69329114b9f84265b99b947674a32f39c03
Arturo Suárez <manuel.suarez@cimat.mx>
2022-08-24 00:29:48 -0500

    Testing model

--- main.py
+++ main.py
@@ -42,4 +42,30 @@ train_generator=generator.flow_from_directory(DATA_GEN_INPUT,
 img = next(train_generator)[0]
 plt.figure(figsize=(5,5))
 plt.imsave('figure1.png', img[:,:,0], cmap='gray')
-plt.colorbar()
+#plt.colorbar()
+
+# Testing model
+def test_model(data_generator, figname):
+    img1,img2=next(data_generator)[:2]
+    noise_var=np.random.rand()*.25
+    # noise_var=.3
+    noisy_img1=random_noise(img1,mode='speckle',var=noise_var,clip=True)
+    noisy_img2=random_noise(img2,mode='speckle',var=noise_var,clip=True)
+    noisy_img1=np.expand_dims(noisy_img1,axis=[0,-1])
+    noisy_img2=np.expand_dims(noisy_img2,axis=[0,-1])
+    denoised_img1=model.predict(noisy_img1)
+    denoised_img2=model.predict(noisy_img2)
+    fig,ax=plt.subplots(3,2,figsize=(10,12))
+    mapple=ax[0,0].imshow(img1[:,:,0])
+    plt.colorbar(mapple,ax=ax[0,0])
+    mapple=ax[0,1].imshow(img2[:,:,0])
+    plt.colorbar(mapple,ax=ax[0,1])
+    mapple=ax[1,0].imshow(noisy_img1[0].reshape(INPUT_SIZE))
+    plt.colorbar(mapple,ax=ax[1,0])
+    mapple=ax[1,1].imshow(noisy_img2[0].reshape(INPUT_SIZE))
+    plt.colorbar(mapple,ax=ax[1,1])
+    mapple=ax[2,0].imshow(denoised_img1[0].reshape(INPUT_SIZE))
+    plt.colorbar(mapple,ax=ax[2,0])
+    mapple=ax[2,1].imshow(denoised_img2[0].reshape(INPUT_SIZE))
+    plt.colorbar(mapple,ax=ax[2,1])
+    plt.savefig(figname)
